7241b42418c94cafbfa880bfd8eda8533a796ec2
natalie-maks <natasha.maxymenko@gmail.com>
2023-05-01 00:20:08 +0100

    Added framer motion animations to Hero component

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { motion } from "framer-motion";
+
 import Image from "next/image";
 import green from "../assets/green.png";
 import yellow from "@/assets/yellow.png";
@@ -6,54 +8,110 @@ import orange from "@/assets/orange.png";
 import blue from "@/assets/blue.png";
 import Button from "./UI/Button";
 
+const MotionButton = motion(Button);
+const MotionImage = motion(Image);
+
 const Hero = () => {
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { opacity: 0, y: -30 },
+    show: { opacity: 1, y: 0, transition: { ease: "easeInOut", duration: 0.7, type: "tween" } },
+  };
+
+  const imgContainer = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        delay: 0.6,
+        staggerChildren: 0.35,
+      },
+    },
+  };
+
+  const imgItem = {
+    hidden: { opacity: 0, x: 20 },
+    show: {
+      opacity: 1,
+      x: 0,
+      transition: { ease: "easeInOut", type: "tween", duration: 1 },
+    },
+  };
+
   return (
     <section
       id="hero"
       className="flex items-center flex-col lg:flex-row gap-20 lg:gap-32 pt-32 mb-40"
     >
-      <div className="lg:w-3/5 flex flex-col justify-center">
-        <h1 className="text-5xl font-medium">Marketing solutions that grow your business.</h1>
-        <p className="mt-10 mb-8">
+      <motion.div
+        variants={container}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+        className="lg:w-3/5 flex flex-col justify-center"
+      >
+        <motion.h1 variants={item} className="text-5xl font-medium">
+          Marketing solutions that grow your business.
+        </motion.h1>
+        <motion.p variants={item} className="mt-10 mb-8">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic ratione quis obcaecati nihil
           odio perspiciatis nisi veritatis eius consequuntur! Iusto nostrum architecto asperiores
           totam fugiat quaerat explicabo rem magnam incidunt.
-        </p>
+        </motion.p>
         <div className="flex flex-wrap gap-6">
-          <Button label={"Our services"} link={"/#services"} scroll={true} />
-          <Button label={"Contact us"} grey={true} link={"/contact"} />
+          <MotionButton variants={item} label={"Our services"} link={"/#services"} scroll={true} />
+          <MotionButton variants={item} label={"Contact us"} grey={true} link={"/contact"} />
         </div>
-      </div>
-      <div className="lg:w-2/5 max-h-[460px] 2xl:max-h-[660px] grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-2 grid-rows-2 sm:grid-rows-none lg:grid-rows-3 lg:gap-x-12 gap-6">
-        <Image
+      </motion.div>
+      <motion.div
+        variants={imgContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+        className="lg:w-2/5 max-h-[460px] 2xl:max-h-[660px] grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-none grid-rows-2 sm:grid-rows-none lg:grid-rows-3 lg:gap-x-12 gap-6"
+      >
+        <MotionImage
+          variants={imgItem}
           width="640"
           height="960"
           alt="smiling woman with tablet"
-          className="w-full h-full object-cover rounded-full rounded-br-none lg:row-span-2"
+          className="h-full w-full object-cover rounded-full rounded-br-none lg:row-start-1 lg:row-end-3"
           src={green}
         />
-        <Image
-          width="640"
-          height="960"
-          alt="smiling man"
-          className="w-full h-full object-cover rounded-full rounded-tr-none"
-          src={blue}
-        />
-        <Image
-          width="640"
-          height="960"
-          alt="smiling man in glasses"
-          className="w-full h-full object-cover rounded-full rounded-tl-none lg:row-span-2"
-          src={orange}
-        />
-        <Image
+        <MotionImage
+          variants={imgItem}
           width="640"
           height="960"
           alt="smiling woman"
-          className="w-full h-full object-cover rounded-full rounded-bl-none "
+          className="h-full w-full object-cover rounded-full rounded-bl-none lg:row-start-3 lg:row-end-4"
           src={yellow}
         />
-      </div>
+        <MotionImage
+          variants={imgItem}
+          width="640"
+          height="960"
+          alt="smiling man"
+          className="h-full w-full object-cover rounded-full rounded-tr-none lg:row-start-1 lg:row-end-2"
+          src={blue}
+        />
+        <MotionImage
+          variants={imgItem}
+          width="640"
+          height="960"
+          alt="smiling man in glasses"
+          className="h-full w-full object-cover rounded-full rounded-tl-none lg:row-start-2 lg:row-end-4"
+          src={orange}
+        />
+      </motion.div>
     </section>
   );
 };
--- components/UI/Button.jsx
+++ components/UI/Button.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Link from "next/link";
 
-const Button = ({ label, link, grey, scroll }) => {
+const Button = React.forwardRef(({ label, link, grey, scroll }, ref) => {
   return (
     <Link
+      ref={ref}
       href={link}
       className={`${grey ? "bg-grey hover:shadow-shadowgrey" : "bg-blue hover:shadow-darkblue"} 
         inline-block text-white text-center py-3 px-6 rounded-full  hover:translate-y-[-2px] shadow-md
@@ -13,6 +14,6 @@ const Button = ({ label, link, grey, scroll }) => {
       {label}
     </Link>
   );
-};
+});
 
 export default Button;
