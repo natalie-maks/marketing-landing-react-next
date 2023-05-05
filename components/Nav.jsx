1dba04038b8bb5f6123df682718b148159aefb3f
natalie-maks <natasha.maxymenko@gmail.com>
2023-05-05 23:24:39 +0100

    Added animations for navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { motion } from "framer-motion";
+
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/assets/logo.png";
@@ -7,33 +9,74 @@ import Button from "./UI/Button";
 const Nav = () => {
   const [clicked, setClicked] = useState(false);
 
+  const navbar = {
+    hidden: { opacity: 0, y: `-100%` },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        ease: "easeInOut",
+        type: "tween",
+        duration: 0.7,
+        staggerChildren: 0.3,
+        delayChildren: 1,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: { ease: "easeInOut", duration: 0.5, type: "tween", staggerChildren: 0.1 },
+    },
+  };
+  const list = {
+    hidden: { opacity: 0, y: -10 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: { ease: "easeInOut", duration: 0.4, type: "tween" },
+    },
+  };
+
   return (
-    <nav className="fixed top-0 z-10 h-20 w-full bg-darkgrey shadow-md shadow-bggrey">
+    <motion.nav
+      initial="hidden"
+      animate="show"
+      variants={navbar}
+      className="fixed top-0 z-10 h-20 w-full bg-darkgrey shadow-md shadow-bggrey"
+    >
       <div className="container flex items-center justify-between h-full px-4 mx-auto">
-        <Link
-          href="/"
-          onClick={() => {
-            window.scrollTo({
-              top: 0,
-              behavior: "smooth",
-            });
-          }}
-          scroll={false}
-          aria-label="Main page"
-        >
-          <Image
-            src={logo}
-            alt="logo"
-            width="100"
-            height="50"
-            className="h-12 w-28 object-scale-down"
-          />
-        </Link>
-        <ul
+        <motion.div variants={item}>
+          <Link
+            href="/"
+            onClick={() => {
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+              });
+            }}
+            scroll={false}
+            aria-label="Main page"
+          >
+            <Image
+              src={logo}
+              alt="logo"
+              width="100"
+              height="50"
+              className="h-12 w-28 object-scale-down"
+            />
+          </Link>
+        </motion.div>
+
+        <motion.ul
+          variants={item}
           className={`${clicked ? "scale-y-100 opacity-100" : "scale-y-0 opacity-0"} 
           absolute lg:relative top-20 lg:top-0 w-full lg:w-auto left-0 lg:h-full py-12 lg:py-0 flex flex-col lg:flex-row items-center justify-end lg:justify-center space-y-6 lg:space-y-0 lg:space-x-2 text-2xl md:text-lg text-white font-sans font-medium bg-grey lg:bg-inherit transition-all origin-top lg:scale-y-100 lg:opacity-100`}
         >
-          <li
+          <motion.li
+            variants={list}
             onClick={() => setClicked(false)}
             className="hover:text-darkblue transition-colors h-full flex items-center border-b-4 px-4 border-transparent hover:border-yellow-400"
           >
@@ -49,35 +92,38 @@ const Nav = () => {
             >
               Home
             </Link>
-          </li>
-          <li
+          </motion.li>
+          <motion.li
+            variants={list}
             onClick={() => setClicked(false)}
             className="hover:text-darkblue transition-colors h-full flex items-center border-b-4 px-4 border-transparent hover:border-yellow-400"
           >
             <Link href="/#about" scroll={false}>
               About
             </Link>
-          </li>
-          <li
+          </motion.li>
+          <motion.li
+            variants={list}
             onClick={() => setClicked(false)}
             className="hover:text-darkblue transition-colors h-full flex items-center border-b-4 px-4 border-transparent hover:border-yellow-400"
           >
             <Link href="/#services" scroll={false}>
               Services
             </Link>
-          </li>
-          <li
+          </motion.li>
+          <motion.li
+            variants={list}
             onClick={() => setClicked(false)}
             className="hover:text-darkblue transition-colors h-full flex items-center border-b-4 px-4 border-transparent hover:border-yellow-400"
           >
             <Link href="/#blog" scroll={false}>
               Blog
             </Link>
-          </li>
-          <li className="hidden lg:block pl-6">
+          </motion.li>
+          <motion.li variants={list} className="hidden lg:block pl-6">
             <Button label={"Get started"} link={"/contact"} />
-          </li>
-        </ul>
+          </motion.li>
+        </motion.ul>
         <button
           className="relative lg:hidden z-10 h-6 w-6"
           aria-label="menu"
@@ -88,7 +134,7 @@ const Nav = () => {
           <span className="bg-white absolute top-[18px] left-0 w-6 h-[3px]"></span>
         </button>
       </div>
-    </nav>
+    </motion.nav>
   );
 };
 
